54daab809979de1aec0e7dbe399fed8967c3d77c
Lucas Dias <lucaalencarID@gmail.com>
2023-07-05 18:31:24 -0300

    bootstrap instalado e pronto para uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import DataVis from './components/dataVisualization/DataVis'
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
   return (
-    <>
+    <main>
       <DataVis/>
-    </>
+    </main>
   )
 }
 
